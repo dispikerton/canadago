6d04c4296bd1be2bb6db029f94a9c48e765f34de
jarvis <dmitrii.zolotenkov@diginetica.com>
2023-03-02 13:31:04 +0400

    init

---
+++ src/main/java/ru/ivanov/canadago/CanadagoApplication.java
@@ -0,0 +1,13 @@
+package ru.ivanov.canadago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanadagoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CanadagoApplication.class, args);
+  }
+
+}
